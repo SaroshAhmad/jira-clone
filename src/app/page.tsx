a29a2e89de253c306f8438571f6e5d3b4c614e97
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-03 14:07:27 +0000

    Update UI components to use these on authentication page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "postcss";
 
 export default function Home() {
   return (
     <div className="">
+
       <Button>
         Primary
       </Button>
